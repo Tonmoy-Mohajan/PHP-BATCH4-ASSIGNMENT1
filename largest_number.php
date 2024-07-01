591e4b710cf4b4ae12392f3c0b3efbfcfb33fea2
TonmoyMj <67537037+TonmoyMj@users.noreply.github.com>
2024-07-01 12:38:44 +0600

    Largest Number

--- largest_number.php
+++ largest_number.php
@@ -1,11 +1,16 @@
 <?php
-$num1=4;
-$num2=5;
 
-if($num1>$num2){
-    Printf("%d is greater than %d",$num1,$num2);
-}else if($num1<$num2){
-    Printf("%d is greater than %d",$num2,$num1);
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+//finding largest number
+if($num1>$num2 && $num1>$num3){
+    printf("%d is greater than %d and %d",$num1,$num2,$num3);
+}else if($num2>$num1 && $num2>$num3){
+    printf("%d is greater than %d and %d",$num2,$num1,$num3);
+}else if($num3>$num1 && $num3>$num2){
+    printf("%d is greater than %d and %d",$num3,$num1,$num2);
 }else{
-    Printf("%d and %d are equal",$num1,$num2);
+    printf("All numbers are equal");
 }
