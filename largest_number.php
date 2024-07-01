033f726c96f88d4d36351ae06f6c590fee5a837f
TonmoyMj <67537037+TonmoyMj@users.noreply.github.com>
2024-07-01 09:47:13 +0600

    Second

---
+++ largest_number.php
@@ -0,0 +1,11 @@
+<?php
+$num1=4;
+$num2=5;
+
+if($num1>$num2){
+    Printf("%d is greater than %d",$num1,$num2);
+}else if($num1<$num2){
+    Printf("%d is greater than %d",$num2,$num1);
+}else{
+    Printf("%d and %d are equal",$num1,$num2);
+}
